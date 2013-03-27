18d96e4053959bf1b85ebaf4ba831e4b89e21bbf
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2013-03-27 13:22:39 -0300

    * immanuel_ben-pack/env/not-so-shiny-now.el: added Django TAGS table

--- immanuel_ben-pack/env/not-so-shiny-now.el
+++ immanuel_ben-pack/env/not-so-shiny-now.el
@@ -13,5 +13,9 @@
 ;; default directory
 (setq default-directory "~/Desktop/")
 
+;; tag tables
+(setq tags-table-list
+           '("/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages/django/TAGS"))
+
 ;; visit tags
 ;(visit-tags-table "/Volumes/PwC-VM/TAGS")
